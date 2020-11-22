4ac2bbb103cacac0002cdcf355a5898fee0f3277
pielloy <pielloy20@gmail.com>
2020-11-22 06:18:17 +0100

    arg

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ export default function App() {
               iconName = 'favorite';
               break;
             case "Profile":
-              iconName = 'account-circle';
+              iconName = 'person';
               break;
             default:
           }
